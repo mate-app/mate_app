014f017ad22cae0d9d4eee81c2ac139e90553c44
Mathis <mathis.fa@gmx.de>
2020-08-27 10:19:46 +0200

    just made the privacy tab without content

--- lib/views/main/einstellungen_datenschutz.dart
+++ lib/views/main/einstellungen_datenschutz.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
-
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:mateapp/models/models.dart';
+import 'package:mateapp/utils/utils.dart';
+import 'package:mateapp/widgets/widgets.dart';
+// TODO: remove import and use inheritance
 import '../../styles/styles.dart';
 
 class EinstellungDatenschutz extends StatefulWidget {
@@ -14,10 +19,61 @@ class _EinstellungDatenschutzState extends State<EinstellungDatenschutz> {
   Widget build(BuildContext context) {
     return Container(
       color: Styles.white,
-      child: const CustomScrollView(
+      child: CustomScrollView(
+        physics: const BouncingScrollPhysics(
+            parent: AlwaysScrollableScrollPhysics()),
         slivers: <Widget>[
-          CupertinoSliverNavigationBar(
-            largeTitle: Text('Datenschutz'),
+          SliverPersistentHeader(
+            delegate: StaticNavigationBar('Datenschutz'),
+            pinned: true,
+            floating: true,
+          ),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            PrivacyText(
+              text: 'Datenschutz',
+            ),
+          ])),
+        ],
+      ),
+    );
+  }
+}
+
+class PrivacyText extends StatelessWidget {
+  final String text;
+
+  //Constructor
+
+  PrivacyText({
+    Key key,
+    this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.fromLTRB(20, 20, 20, 40),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(5, 25, 5, 0),
+            child: Text(
+              text,
+              style: Styles.font.apply(color: Styles.grey),
+              // textAlign: TextAlign.justify,
+            ),
+          ),
+          Center(
+            child: Container(
+              margin: EdgeInsets.fromLTRB(0, 0, 0, 50),
+              child: CupertinoButton(
+                padding: EdgeInsets.zero,
+                child: Text('zur Webseite'),
+                onPressed: () => launchURL('www.mate-app.de/datenschutz'),
+              ),
+            ),
           ),
         ],
       ),
