a9ead5773714fff0be97d6b0a195ebe188ef1dee
Tobias Pörtner <t.poertner@outlook.com>
2020-09-25 17:57:49 +0200

    commented bad code out

--- lib/services/globals.dart
+++ lib/services/globals.dart
@@ -9,10 +9,10 @@ mixin Global {
     Dish: (String id, data) => Dish.fromMap(id, data),
     Event: (String id, data) => Event.fromMap(id, data),
     Article: (String id, data) => Article.fromMap(id, data),
-    Subject: (String id, data) => Subject.fromMap(id, data),
-    University: (String id, data) => University.fromMap(id, data),
-    UserModel: (String id, data) => UserModel.fromMap(id, data),
-    Grade: (String id, data) => Grade.fromMap(id, data),
+    // Subject: (String id, data) => Subject.fromMap(id, data),
+    // University: (String id, data) => University.fromMap(id, data),
+    // UserModel: (String id, data) => UserModel.fromMap(id, data),
+    // Grade: (String id, data) => Grade.fromMap(id, data),
   };
 
   static final UserData userModelRef = UserData(collection: 'users');
