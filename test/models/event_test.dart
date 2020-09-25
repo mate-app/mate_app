39d129e02a736674057e83b2c9ab68688e50e9b4
Tobias Pörtner <t.poertner@outlook.com>
2020-09-25 18:12:43 +0200

    another typo

--- test/models/event_test.dart
+++ test/models/event_test.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:mateapp/models/Event.dart';
+import 'package:mateapp/models/event.dart';
 
 void main() {
   group('Test Event Model', () {
