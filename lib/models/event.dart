5e53ad91caa9a9ebb8deae604b9d3df7e4439abf
Tobias Pörtner <t.poertner@outlook.com>
2020-09-25 17:48:26 +0200

    event_test done

--- lib/models/event.dart
+++ lib/models/event.dart
@@ -1,103 +1,107 @@
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:intl/intl.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:mateapp/utils/utils.dart';
 
 class Event {
-  String id;
-  String courseName;
+  String _id;
+  String _courseName;
   DateTime date;
-  String degree;
+  String _degree;
   DateTime endsAt;
-  List<String> groups;
-  List<String> lecturers;
-  String location;
-  String major;
-  int semester;
+  List<String> _groups;
+  List<String> _lecturers;
+  String _location;
+  String _major;
+  int _semester;
   DateTime startsAt;
-  List<String> subjects;
-  String type;
+  List<String> _subjects;
+  String _type;
 
-  Event(
-      {this.id,
-      this.courseName,
-      this.date,
-      this.degree,
-      this.endsAt,
-      this.groups,
-      this.lecturers,
-      this.location,
-      this.major,
-      this.semester,
-      this.startsAt,
-      this.subjects,
-      this.type});
-  Event.fromMap(doc) {
-    id = doc.id is String ? doc.id as String : '';
-    courseName = doc.data()['course_name'] is String
-        ? doc.data()['course_name'] as String
-        : '';
-    date = doc.data()['date'].toDate() is DateTime
-        ? doc.data()['date'].toDate() as DateTime
-        : DateTime.utc(1970);
-    degree =
-        doc.data()['degree'] is String ? doc.data()['degree'] as String : '';
-    endsAt = doc.data()['ends_at'].toDate() is DateTime
-        ? doc.data()['ends_at'].toDate() as DateTime
-        : DateTime.utc(1970);
-    groups = doc.data()['groups'] is List<String>
-        ? List.from(doc.data()['groups'] as List<String>)
-        : [];
-    lecturers = doc.data()['lecturers'] is List<String>
-        ? List.from(doc.data()['lecturers'] as List<String>)
-        : [];
-    location = doc.data()['location'] is String
-        ? doc.data()['location'] as String
-        : '';
-    major = doc.data()['major'] is String ? doc.data()['major'] as String : '';
-    semester =
-        doc.data()['semester'] is int ? doc.data()['semester'] as int : 0;
-    startsAt = doc.data()['starts_at'].toDate() is DateTime
-        ? doc.data()['starts_at'].toDate() as DateTime
-        : DateTime.utc(1970);
-    subjects = doc.data()['subjects'] is List<String>
-        ? List.from(doc.data()['subjects'] as List<String>)
-        : [];
-    type = doc.data()['event_type'] is String
-        ? doc.data()['event_type'] as String
-        : '';
+  Event({
+    String id,
+    String courseName,
+    this.date,
+    String degree,
+    this.endsAt,
+    List<String> groups,
+    List<String> lecturers,
+    String location,
+    String major,
+    int semester,
+    this.startsAt,
+    List<String> subjects,
+    String type,
+  })  : _id = id,
+        _courseName = courseName,
+        _degree = degree,
+        _groups = groups,
+        _lecturers = lecturers,
+        _location = location,
+        _major = major,
+        _semester = semester,
+        _subjects = subjects,
+        _type = type;
+
+  Event.fromMap(String id, data) : assert(data != null) {
+    id = id is String ? id : null;
+    courseName =
+        data['course_name'] is String ? data['course_name'] as String : null;
+    date = data['date'] != null && data['date'] is Timestamp
+        ? data['date'].toDate() as DateTime
+        : null;
+    degree = data['degree'] is String ? data['degree'] as String : null;
+    endsAt = data['ends_at'] != null && data['ends_at'] is Timestamp
+        ? data['ends_at'].toDate() as DateTime
+        : null;
+    groups = data['groups'] is List
+        ? List.from(data['groups'] as List<String>)
+        : null;
+    lecturers = data['lecturers'] is List
+        ? List.from(data['lecturers'] as List<String>)
+        : null;
+    location = data['location'] is String ? data['location'] as String : null;
+    major = data['major'] is String ? data['major'] as String : null;
+    semester = data['semester'] is int ? data['semester'] as int : null;
+    startsAt = data['starts_at'] != null && data['starts_at'] is Timestamp
+        ? data['starts_at'].toDate() as DateTime
+        : null;
+    subjects = data['subjects'] is List<String>
+        ? List.from(data['subjects'] as List<String>)
+        : null;
+    type = data['event_type'] is String ? data['event_type'] as String : null;
   }
 
-  String get getStartsAt {
-    return removeDateFromDateTime(startsAt);
-  }
+  set id(String id) => _id = id;
+  set courseName(String courseName) => _courseName = courseName;
+  set degree(String degree) => _degree = degree;
+  set groups(List<String> groups) => _groups = groups;
+  set lecturers(List<String> lecturers) => _lecturers = lecturers;
+  set location(String location) => _location = location;
+  set major(String major) => _major = major;
+  set semester(int semester) => _semester = semester;
+  set subjects(List<String> subjects) => _subjects = subjects;
+  set type(String type) => _type = type;
 
-  String get getEndsAt {
-    return removeDateFromDateTime(endsAt);
-  }
+  String get id => _id ?? '';
+  String get courseName => _courseName ?? '';
+  String get degree => _degree ?? '';
+  List<String> get groups => _groups ?? [];
+  List<String> get lecturers => _lecturers ?? [];
+  String get location => _location ?? '';
+  String get major => _major ?? '';
+  int get semester => _semester ?? 0;
+  List<String> get subjects => _subjects ?? [];
+  String get type => _type ?? '';
 
-  String get getDate {
-    return convertDateToString(date);
-  }
+  String get getStartsAt => removeDateFromDateTime(startsAt);
+  String get getEndsAt => removeDateFromDateTime(endsAt);
+  String get getDate => convertDateToString(date);
 
-  dynamic get getType {
-    final Map shortType = {
-      'Vorlesung': 'VL', // orange
-      'Seminar': 'SE', // rot
-      'Übung': 'Ü', // rot
-      'Wahlmodul': 'WAHL', // türkis
-      'Praxis': 'Pr', // rot
-      'Vorlesung/Labor': 'VL/L' // Orange
-    };
-    return shortType[type];
-  }
-
-  String convertDateToString(DateTime datetime) {
-    initializeDateFormatting('de_DE');
-    return DateFormat('dd. MMM y', 'de_DE')
-        .format(DateTime.parse(datetime.toString()));
-  }
-
-  String removeDateFromDateTime(DateTime datetime) {
-    initializeDateFormatting('de_DE');
-    return DateFormat('', 'de_DE').add_Hm().format(datetime);
-  }
+  dynamic get getType => {
+        'Vorlesung': 'VL', // orange
+        'Seminar': 'SE', // rot
+        'Übung': 'Ü', // rot
+        'Wahlmodul': 'WAHL', // türkis
+        'Praxis': 'Pr', // rot
+        'Vorlesung/Labor': 'VL/L' // Orange
+      }[type];
 }
---
+++ test/models/event_test.dart
@@ -0,0 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mateapp/models/Event.dart';
+
+void main() {
+  group('Test Event Model', () {
+    test('Should return a valid Event instance', () {
+      final mockData = {
+        'course_name': 'courseName',
+        'groups': ['A'],
+        'date': Timestamp.now(),
+        'semester': 4
+      };
+
+      expect(Event.fromMap('id', mockData), isInstanceOf<Event>());
+      expect(Event.fromMap('id', mockData).courseName, 'courseName');
+      expect(Event.fromMap('id', mockData).semester, 4);
+      expect(Event.fromMap('id', mockData).groups, ['A']);
+      expect(Event.fromMap('id', mockData).date, isInstanceOf<DateTime>());
+    });
+
+    test('Should return empty string when using wrong type', () {
+      final mockData = {
+        'courseName': 3,
+      };
+      expect(Event.fromMap('id', mockData).courseName, '');
+    });
+
+    test('Should throw AssertionError, if null given to named constructor', () {
+      expect(() => Event.fromMap(null, null), throwsA(isA<AssertionError>()));
+    });
+
+    test('Should return empty Event if no parameters are used', () {
+      expect(Event(), isInstanceOf<Event>());
+      expect(Event().semester, 0);
+      expect(Event().groups, []);
+      expect(Event().date, null);
+    });
+  });
+}
