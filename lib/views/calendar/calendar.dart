970ba4ed56ec4392fdbe1b78d7ad1982c50c925e
Tobias Pörtner <t.poertner@outlook.com>
2020-11-04 13:33:35 +0100

    backfallscreens implemented, but two missing

--- lib/views/calendar/calendar.dart
+++ lib/views/calendar/calendar.dart
@@ -18,15 +18,67 @@ class Calendar extends StatelessWidget {
               stream: EventStream(user: user).stream,
               builder:
                   (BuildContext context, AsyncSnapshot<List<Event>> snapshot) {
-                if (snapshot.hasData) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData) {
                   return CalendarList(events: snapshot.data);
+                }
+                if (snapshot.connectionState == ConnectionState.done &&
+                    !snapshot.hasData) {
+                  return FutureBuilder<Subject>(
+                    future: Document<Subject>(
+                            path:
+                                'hochschulen/${user.university}/${user.subject}')
+                        .getData(),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<Subject> snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done &&
+                          snapshot.hasData) {
+                        if (snapshot.data.supported) {
+                          return SliverToBoxAdapter(
+                            child: Container(
+                              height: MediaQuery.of(context).size.height * 0.85,
+                              decoration: const BoxDecoration(
+                                image: DecorationImage(
+                                  image: AssetImage(
+                                      'assets/backfall/mensa_backfall.png'),
+                                ),
+                              ),
+                            ),
+                          );
+                        }
+                      }
+                      return SliverToBoxAdapter(
+                        child: Container(
+                          height: MediaQuery.of(context).size.height * 0.85,
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage(
+                                  'assets/backfall/termine_backfall_notsupported.png'),
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  );
                 }
                 return const SliverLoadingIndicator();
               },
             );
     }
     return SliverToBoxAdapter(
-      child: RegisterButton(),
+      child: Column(
+        children: [
+          RegisterButton(),
+          Container(
+            height: MediaQuery.of(context).size.height * 0.6,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/backfall/mensa_backfall.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
